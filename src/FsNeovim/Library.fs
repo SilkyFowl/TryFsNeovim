e6a2ed85bcd4665c34731ca464379614a51895a4
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2021-05-11 02:08:54 +0000

    FIx CI Error Fixes #1

--- src/FsNeovim/Library.fs
+++ src/FsNeovim/Library.fs
@@ -1,5 +1,147 @@
-﻿namespace FsNeovim
+﻿module FsNeovim.MessagePackRpc
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open MessagePack
+open MessagePack.FSharp
+open MessagePack.Resolvers
+open System.Buffers
+open System.IO
+open System.IO.Pipelines
+
+open type MessagePack.MessagePackSerializer
+open Microsoft.FSharp.Core.LanguagePrimitives
+
+type TypeId =
+    | Request = 0uy
+    | Response = 1uy
+    | Notification = 2uy
+
+type Request<'TParam> =
+    { msgid: uint
+      method: string
+      parameters: 'TParam seq }
+
+type Response<'TResult, 'TError> =
+    { msgid: uint
+      error: 'TError
+      result: 'TResult }
+
+type Notification<'TParam> =
+    { method: string
+      parameters: 'TParam seq }
+
+[<MessagePackObject>]
+type RequestMessage<'TParam> =
+    { [<Key(0)>]
+      typeid: byte
+      [<Key(1)>]
+      msgid: uint
+      [<Key(2)>]
+      method: string
+      [<Key(3)>]
+      parameters: 'TParam seq }
+
+[<MessagePackObject>]
+type ResponseMessage<'TResult, 'TError> =
+    { [<Key(0)>]
+      typeid: byte
+      [<Key(1)>]
+      msgid: uint
+      [<Key(2)>]
+      error: 'TError
+      [<Key(3)>]
+      result: 'TResult }
+
+[<MessagePackObject>]
+type NotificationMessage<'TParam> =
+    { [<Key(0)>]
+      typeid: byte
+      [<Key(1)>]
+      method: string
+      [<Key(2)>]
+      parameters: 'TParam seq }
+
+type MsgPackRpcDto<'TParam, 'TResult, 'TError> =
+    | Request of Request<'TParam>
+    | Response of Response<'TResult, 'TError>
+    | Notification of Notification<'TParam>
+
+    static member ofMessagePackObject(reqest: RequestMessage<'TParam>) =
+        Request
+            { msgid = reqest.msgid
+              method = reqest.method
+              parameters = reqest.parameters }
+
+    static member ofMessagePackObject(responce: ResponseMessage<'TResult, 'TError>) =
+        Response
+            {  msgid = responce.msgid
+               error = responce.error
+               result = responce.result }
+
+    static member ofMessagePackObject(notification: NotificationMessage<'TParam>) =
+        Notification
+            {  method = notification.method
+               parameters = notification.parameters }
+
+
+/// Wrapper function. Serialize into IBufferWriter.
+let serialize<'T> (writer: IBufferWriter<byte>) options cancellationToken (value: 'T) =
+    Serialize<'T>(writer, value, options, cancellationToken)
+
+
+let deserialize<'T> (byteSequence: inref<ReadOnlySequence<byte>>) options cancellationToken =
+    Deserialize<'T>(&byteSequence, options, cancellationToken)
+
+let private options =
+    Resolvers.CompositeResolver.Create(FSharpResolver.Instance, StandardResolver.Instance)
+    |> MessagePackSerializerOptions.Standard.WithResolver
+
+/// Serialize into IBufferWriter.
+let serializeMessage writer cancellationToken value =
+    match value with
+    | Request req ->
+        { typeid = byte TypeId.Request
+          msgid = req.msgid
+          method = req.method
+          parameters = req.parameters }
+        |> serialize writer options cancellationToken
+
+    | Response res ->
+        { typeid = byte TypeId.Response
+          msgid = res.msgid
+          error = res.error
+          result = res.result }
+        |> serialize writer options cancellationToken
+
+    | Notification notify ->
+        { typeid = byte TypeId.Notification
+          method = notify.method
+          parameters = notify.parameters }
+        |> serialize writer options cancellationToken
+
+
+let deserializeMessage<'TParam, 'TResult, 'TError> (byteSequence: inref<ReadOnlySequence<byte>>) cancellationToken =
+    let typeid : TypeId =
+        EnumOfValue(byteSequence.Slice(1, 1).ToArray().[0])
+
+    match typeid with
+    | TypeId.Request ->
+        deserialize<RequestMessage<'TParam>> &byteSequence options cancellationToken
+        |> MsgPackRpcDto<'TParam, 'TResult, 'TError>
+            .ofMessagePackObject
+
+    | TypeId.Response ->
+        deserialize<ResponseMessage<'TResult, 'TError>> &byteSequence options cancellationToken
+        |> MsgPackRpcDto<'TParam, 'TResult, 'TError>
+            .ofMessagePackObject
+
+    | TypeId.Notification ->
+        deserialize<NotificationMessage<'TParam>> &byteSequence options cancellationToken
+        |> MsgPackRpcDto<'TParam, 'TResult, 'TError>
+            .ofMessagePackObject
+
+    | _ -> invalidArg "byteSequence" "typeid is wrong."
+
+let getWriter (stream: Stream) =
+    PipeWriter.Create(stream, new StreamPipeWriterOptions(leaveOpen = true))
+
+let getReader stream = new MessagePackStreamReader(stream)
--- tests/FsNeovim.Tests/Sample.fs
+++ tests/FsNeovim.Tests/Sample.fs
@@ -1,36 +1,116 @@
 module Tests
 
 open Expecto
+open Expecto.Flip
+open FsCheck
+open FsNeovim.MessagePackRpc
+open System.IO
+
+let convert (stream: Stream) token value =
+    async {
+        let writer = getWriter stream
+        let reader = getReader stream
+
+        Request value |> serializeMessage writer token
+        do! writer.FlushAsync().AsAsync() |> Async.Ignore
+
+        stream.Seek(0L, SeekOrigin.Begin) |> ignore
+
+        let! readResult = reader.ReadAsync(token).AsAsync()
+        let seq = readResult.Value
+        let dto = deserializeMessage &seq token
+
+        match dto with
+        | Request x -> return x
+        | _ -> return failwith "Convert Error"
+    }
+
+type RequestGen<'T> =
+    static member Request() : Arbitrary<_> =
+        let genMsgid = Arb.generate<uint>
+        let genMethod = Arb.generate<string>
+        let genParameters = Arb.generate<'T> |> Gen.listOf
+
+        let create msgid method parameters =
+            { msgid = msgid
+              method = method
+              parameters = parameters }
+
+        let genReqest =
+            create <!> genMsgid
+            <*> genMethod
+            <*> genParameters
+
+        genReqest |> Arb.fromGen
+
+let reqConfig<'T> =
+    { FsCheckConfig.defaultConfig with
+          maxTest = 10000
+          arbitrary = [ typeof<RequestGen<'T>> ] }
+
+let testReqProp<'T when 'T: equality> name =
+    testPropertyWithConfig reqConfig<'T> name
+    <| fun (req: Request<'T>) ->
+        async {
+            let stream = new MemoryStream()
+            let token = Async.DefaultCancellationToken
+
+            let! actual = req |> convert stream token
+
+            actual.msgid
+            |> Expect.equal "id should be equal" req.msgid
+
+            actual.method
+            |> Expect.equal "method should be equal" req.method
+
+            actual.parameters
+            |> Expect.sequenceEqual "params should be equal" req.parameters
+        }
+        |> Async.RunSynchronously
+
+let testReqPropError<'T when 'T: equality> name errorTest =
+    testPropertyWithConfig reqConfig<'T> name
+    <| fun (req: Request<'T>) ->
+        async {
+            let stream = new MemoryStream()
+            let token = Async.DefaultCancellationToken
+
+            let! actual = req |> convert stream token
+
+            actual.msgid
+            |> Expect.equal "id should be equal" req.msgid
+
+            actual.method
+            |> Expect.equal "method should be equal" req.method
+
+            try
+                actual.parameters
+                |> Expect.sequenceEqual "params should be equal" req.parameters
+            with :? AssertException as err -> Seq.iter2 errorTest req.parameters actual.parameters
+        }
+        |> Async.RunSynchronously
+
 
 [<Tests>]
 let tests =
-  testList "samples" [
-    testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
-      let subject = true
-      Expect.isTrue subject "I compute, therefore I am."
+    testList "message"
+    <| [ testReqProp<int> "Request<int> Test"
+         testReqProp<string> "Request<string> Test"
+         testReqProp<bool> "Request<bool> Test"
+         testReqProp<byte> "Request<byte> Test"
 
-    testCase "when true is not (should fail)" <| fun _ ->
-      let subject = false
-      Expect.isTrue subject "I should fail because the subject is false"
+         testReqPropError<_> "Issue: Request<_> deserialize not work"
+         <| fun expect actual ->
+             if expect <> null
+                && expect.GetType() <> actual.GetType() then
+                 actual.GetType()
+                 |> Expect.equal "may cause parameters to be of type byte." typeof<byte>
 
-    testCase "I'm skipped (should skip)" <| fun _ ->
-      Tests.skiptest "Yup, waiting for a sunny day..."
+         testReqPropError<obj> "Issue: Request<obj> deserialize not work"
+         <| fun expect actual ->
+             if expect <> null
+                && expect.GetType() <> actual.GetType() then
+                 actual.GetType()
+                 |> Expect.equal "may cause parameters to be of type byte." typeof<byte>
 
-    testCase "I'm always fail (should fail)" <| fun _ ->
-      Tests.failtest "This was expected..."
-
-    testCase "contains things" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4 |]
-                         "This is the case; {2,3,4} contains {2,4}"
-
-    testCase "contains things (should fail)" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4; 1 |]
-                         "Expecting we have one (1) in there"
-
-    testCase "Sometimes I want to ༼ノಠل͟ಠ༽ノ ︵ ┻━┻" <| fun _ ->
-      Expect.equal "abcdëf" "abcdef" "These should equal"
-
-    test "I am (should fail)" {
-      "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal true false
-    }
-  ]
+         ]
